186746263f15b84d576cbc8111d94504110dfd89
ditinagrawal <ditin.agrawal05@gmail.com>
2024-07-13 10:25:55 +0530

    Variables and DataTypes

---
+++ 01_variables_and_datatypes/index.js
@@ -0,0 +1,49 @@
+// Activity - 1
+
+var age = 19;
+console.log(age);
+
+let my_name = "Ditin Agrawal";
+console.log(my_name);
+
+// Activity - 2
+
+const isLearning = true;
+console.log(isLearning);
+
+// Activity - 3
+
+const user = {
+    name: "Ditin",
+    age: 19,
+    isLearning: true,
+    instructor: "Hitesh Choudhary Sir",
+};
+
+const my_heroes = [
+    "My Dad",
+    "My Mom",
+    "My Brother",
+    "Hitesh Choudhary Sir",
+    "IronMan",
+];
+
+console.log(typeof age);
+console.log(typeof my_name);
+console.log(typeof isLearning);
+console.log(typeof user);
+console.log(typeof my_heroes);
+
+// Activity - 4
+my_name = "Name Changed to Dev Jindal";
+console.log(my_name);
+
+// Activity - 5
+
+isLearning = false; // TypeError: Assignment to constant variable.
+console.log(isLearning);
+
+// Difference between let and var
+
+// var is function scoped and let is block scoped variable.
+// var can be hoisted but let can't be hoisted.
