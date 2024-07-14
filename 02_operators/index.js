7f6799257735f10aa358472747fe4485d4b53759
ditinagrawal <ditin.agrawal05@gmail.com>
2024-07-14 10:42:39 +0530

    Operators

---
+++ 02_operators/index.js
@@ -0,0 +1,49 @@
+// Activity - 1
+let a = 50;
+let b = 20;
+
+let sum = a + b;
+let diff = a - b;
+let mul = a * b;
+let div = a / b;
+let mod = a % b;
+console.log("Sum: ", sum);
+console.log("Difference: ", diff);
+console.log("Multiplication: ", mul);
+console.log("Division: ", div);
+console.log("Modulus: ", mod);
+
+// Activity - 2
+let x = 10;
+x += 5;
+console.log("x += 5: ", x);
+
+x -= 5;
+console.log("x -= 5: ", x);
+
+// Activity - 3
+
+let num1 = 10;
+let num2 = 20;
+
+console.log("num1 < num2: ", num1 < num2);
+console.log("num1 > num2: ", num1 > num2);
+console.log("num1 <= num2: ", num1 <= num2);
+console.log("num1 >= num2: ", num1 >= num2);
+console.log("num1 == num2: ", num1 == num2);
+console.log("num1 === num2: ", num1 === num2);
+
+// Activity - 4
+
+let isCoder = true;
+let isLearning = true;
+let isWorking = false;
+
+console.log("isCoder && isLearning: ", isCoder && isLearning);
+console.log("isCoder || isLearning: ", isCoder || isWorking);
+console.log("!isCoder: ", !isCoder);
+
+// Activity - 5
+
+let number = 10;
+number >= 0 ? console.log("Positive") : console.log("Negative");
